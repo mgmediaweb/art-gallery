579952e49266e1ee1ee6ffe4d65de25b32a94b06
darikmohammed <r.m.darik@gmail.com>
2022-07-25 13:41:31 +0300

    Add i18next localizatino

--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import i18n from 'i18next';
+import { useTranslation, initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    fallbackLng: 'en',
+    detection: {
+      order: [
+        'path',
+        'cookie',
+        'htmlTag',
+        'localStorage',
+        'sessionStorage',
+        'subdomain',
+      ],
+      caches: ['cookie'],
+    },
+    interpolation: {
+      escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+    },
+    backend: {
+      loadPath: '/assets/locals/{{lng}}/translation.json',
+    },
+  });
+
+function App() {
+  const { t } = useTranslation();
+
+  return <h2>{t('Welcome_to_react')}</h2>;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +45,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
