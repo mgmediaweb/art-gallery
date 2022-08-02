f7625a3deb75654cdcc2a6c0be615d5660dcd95e
Gonzalo <mgmediaweb@gmail.com>
2022-08-02 13:34:46 -0700

    add bg image and details

--- src/routes/VirtualGallery.jsx
+++ src/routes/VirtualGallery.jsx
@@ -1,11 +1,12 @@
 import { useParams } from 'react-router-dom';
+import bgdemo from '../assets/scenarios/demo.jpg';
 
 const VirtualGalleryScreen = () => {
   const { id } = useParams();
   console.log(id);
 
   return (
-    <section>
+    <section style={{ backgroundImage: `url(${bgdemo})` }}>
       <h1>Virtual Gallery Here</h1>
     </section>
   );
