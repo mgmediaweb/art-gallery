4825acd8946700dab6cab2741f62d7d571985f6b
darikmohammed <r.m.darik@gmail.com>
2022-08-06 01:12:20 +0300

    Implement the translation for English and Espanol in Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,34 +1,56 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import i18next from 'i18next';
+
 import logo from '../../assets/images/logo.png';
 import './Navbar.scss';
 
-const Navbar = () => (
-  <nav>
-    <ul>
-      <li className="logo">
-        <img src={logo} alt="Gallery Logo" className="logo-main" />
-      </li>
-      <li>
-        <NavLink to="/">Obras</NavLink>
-      </li>
-      <li>
-        <NavLink to="/artist">Aristas</NavLink>
-      </li>
-      <li>
-        <NavLink to="/gallery">Galeria</NavLink>
-      </li>
-      <li>
-        <NavLink to="/contact">Contacto</NavLink>
-      </li>
-      <li>
-        <NavLink to="/guest">Invitado</NavLink>
-      </li>
-    </ul>
-    <div className="bottom-nav">
-      <div className="language">Espanol</div>
-      <div className="currency">MXP</div>
-    </div>
-  </nav>
-);
+const Navbar = () => {
+  const { t } = useTranslation();
+  const [EnglishLanguage, setEnglishLanguage] = useState(true);
+
+  return (
+    <nav>
+      <ul>
+        <li className="logo">
+          <img src={logo} alt="Gallery Logo" className="logo-main" />
+        </li>
+        <li>
+          <NavLink to="/">{t('construction')}</NavLink>
+        </li>
+        <li>
+          <NavLink to="/artist">{t('edges')}</NavLink>
+        </li>
+        <li>
+          <NavLink to="/gallery">{t('gallery')}</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">{t('contact')}</NavLink>
+        </li>
+        <li>
+          <NavLink to="/guest">{t('guest')}</NavLink>
+        </li>
+      </ul>
+      <div className="bottom-nav">
+        <button
+          type="button"
+          className="language"
+          onClick={() => {
+            if (EnglishLanguage) {
+              i18next.changeLanguage('es');
+            } else {
+              i18next.changeLanguage('en');
+            }
+            setEnglishLanguage(!EnglishLanguage);
+          }}
+        >
+          {EnglishLanguage ? 'English' : 'Espanol'}
+        </button>
+        <div className="currency">{t('currency')}</div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
